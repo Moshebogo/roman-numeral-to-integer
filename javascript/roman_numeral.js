4e7fe49cc331c730a0fa0db9b33ace93da9095dc
Eli Moshe <moshebogo@gmail.com>
2023-05-04 04:45:49 -0400

    some idea

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,24 +1,59 @@
 function romanNumeral(string) {
-  // type your code here
-}
+  // all posibble combos 
+  const dictionary = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500
+  }
+  // turn each letter into a number
+  const array = []
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 1");
-  console.log(romanNumeral('I'));
+   for (let i = 0; i < string.length; i++) {
+    const currentLetter = dictionary[string[i]]
+    const nextLetter = dictionary[string[i+1]] 
 
-  console.log("");
+    if (dictionary[currentLetter] > dictionary[nextLetter]) {
+      console.log("curr =>", dictionary[currentLetter])
+      array.push(dictionary[currentLetter])
+    } else if (dictionary[currentLetter] < dictionary[nextLetter]) {
+      console.log("next =>", dictionary[nextLetter])
+      array.push(dictionary[nextLetter] - dictionary[currentLetter])
+    }
+   }
 
-  console.log("Expecting: 9");
-  console.log(romanNumeral('IX'));
+   console.log('array =>', array)
+   
+    // add sum of all numbers 
+    let total = 0
+    for (num of array) {
+     total += num
+    }
+    // return total sum
+    // return total
+  }
 
-  console.log("");
+romanNumeral("VVI")
 
-  console.log("Expecting: 402");
-  console.log(romanNumeral('CDII'));
-}
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: 1");
+//   console.log(romanNumeral('I'));
 
-module.exports = romanNumeral;
+//   console.log("");
+
+//   console.log("Expecting: 9");
+//   console.log(romanNumeral('IX'));
+
+//   console.log("");
+
+//   console.log("Expecting: 402");
+//   console.log(romanNumeral('CDII'));
+// }
+
+// module.exports = romanNumeral;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
