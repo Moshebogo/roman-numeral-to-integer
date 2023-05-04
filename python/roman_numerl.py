9e9fe1202d19365d5de56389d75d945ff8760eae
Eli Moshe <moshebogo@gmail.com>
2023-05-04 12:00:52 -0400

    all python passing

--- python/roman_numerl.py
+++ python/roman_numerl.py
@@ -11,25 +11,28 @@ def roman_numeral_converter(string):
    }
 
    array = []
+   pass_letter = False
 
    for i in range(len(string)):
        # with current_letter and next_letter the rest is easier  
        curent_letter = dictionary[string[i]]
-       print("current =>", curent_letter)
        try:
            next_letter = dictionary[string[i+1]]
        except IndexError:
            next_letter = 0
        # decides what to append based on roman numeral rules
-       if curent_letter > next_letter:
+       if pass_letter == True:
+           pass_letter = False
+           continue
+       elif curent_letter > next_letter:
            array.append(curent_letter)
        elif curent_letter == next_letter:
            array.append(curent_letter)
        elif curent_letter < next_letter:
            array.append(next_letter - curent_letter)
+           pass_letter = True
            i+1    
    
-       print("array=>", array)    
        total = 0
        for number in array:
            total += number
@@ -39,4 +42,17 @@ def roman_numeral_converter(string):
 
 
 if __name__ == '__main__':
+    print("expect: 4")
     print(roman_numeral_converter("IV"))
+    print()
+    print("expect: 223")
+    print(roman_numeral_converter("CCXXIII"))
+    print()
+    print("expect: 574")
+    print(roman_numeral_converter("DLXXIV"))
+    print()
+    print("expect: 1999")
+    print(roman_numeral_converter("MCMXCIX"))
+    print()
+    print("expect: 3848")
+    print(roman_numeral_converter("MMMDCCCXLVIII"))
