bd6b8301ef357c68516cf8585827558a339837b2
Eli Moshe <moshebogo@gmail.com>
2023-05-04 10:52:01 -0400

    created python ; git push

---
+++ python/roman_numerl.py
@@ -0,0 +1,20 @@
+def roman_numeral_converter(string):
+   
+   dictionary = {
+      'I': 1,
+      "V": 5,
+      "X": 10,
+      "L": 50,
+      "C": 100,
+      "D": 500,
+      "M": 1000
+   }
+
+
+
+    return string
+
+
+
+if __name__ == '__main__':
+    print(roman_numeral_converter())
