d1e1a775053d65ba3945ac3fc96e2ae22f145c59
Eli Moshe <moshebogo@gmail.com>
2023-05-04 11:16:53 -0400

    2 out of 3 cases passing

--- python/roman_numerl.py
+++ python/roman_numerl.py
@@ -10,11 +10,31 @@ def roman_numeral_converter(string):
       "M": 1000
    }
 
+   array = []
 
+   for i in range(len(string)):
+       # with current_letter and next_letter the rest is easier  
+       curent_letter = dictionary[string[i]]
+       print("current =>", curent_letter)
+       try:
+           next_letter = dictionary[string[i+1]]
+       except IndexError:
+           next_letter = 0
+       # decides what to append based on roman numeral rules
+       if curent_letter > next_letter:
+           array.append(curent_letter)
+       elif curent_letter == next_letter:
+           array.append(curent_letter)
+   
+   
+           
+       total = 0
+       for number in array:
+           total += number
 
-    return string
+   return total
 
 
 
 if __name__ == '__main__':
-    print(roman_numeral_converter())
+    print(roman_numeral_converter("VI"))
